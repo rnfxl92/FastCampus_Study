34ab79202ae45b6b02eb0c7565d251d89a84b00f
SungminPark <rnfxl92@naver.com>
2023-11-22 22:40:08 +0900

    프로젝트 생성

---
+++ VoiceMemo/VoiceMemo/VoiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceMemoApp.swift
+//  VoiceMemo
+//
+//  Created by 박성민 on 11/19/23.
+//
+
+import SwiftUI
+
+@main
+struct VoiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
