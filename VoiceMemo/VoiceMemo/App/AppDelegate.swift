8fb1a9b1609532bdba71ec2f2e69e22b1926c2df
SungminPark <rnfxl92@naver.com>
2023-11-23 23:45:32 +0900

    기초 세팅

---
+++ VoiceMemo/VoiceMemo/App/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  AppDelegate.swift
+//  VoiceMemo
+//
+//  Created by 박성민 on 11/23/23.
+//
+
+import UIKit
+
+class AppDelegate {
+    
+}
---
+++ VoiceMemo/VoiceMemo/Feature/Onboarding/OnboardingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  OnboardingViewModel.swift
+//  VoiceMemo
+//
+//  Created by 박성민 on 11/23/23.
+//
+
+import Foundation
+
+class OnboardingViewModel {
+    
+}
